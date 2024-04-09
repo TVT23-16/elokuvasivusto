5b91510d6fc7621c6c8a89071f4b9c17fa7c67da
dajaakko <jaakko.kaurala@hotmail.fi>
2024-04-09 20:33:38 +0300

    käyttäjän luonti route tehty, tietokantaa muokkailtu, delete route tehty

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,4 @@
-const { getUsers, addUser } = require("../database/user_db");
+const { getUsers, addUser, delUser } = require("../database/user_db");
 
 const router = require("express").Router();
 
@@ -8,11 +8,13 @@ router.get("/all", async (req,res) => {
     res.json(users);
 })
 
-router.post("/add", async (req,res) => {
-        console.log(req.body);
-        await addUser(req.body.password); // Kutsutaan addUser-funktiota uuden käyttäjän lisäämiseksi
-        console.log(req.body.password);
-        res.status(200).json({ message: "Käyttäjä lisätty onnistuneesti." }); // Lähetetään vastaus onnistuneesta lisäyksestä
-    })
 
+
+    router.delete("/delete/:usernam", async (req,res) => {
+        console.log(req.params);
+        const usernam = req.params.usernam
+        await delUser(usernam)
+        res.status(200).json({message:"käyttäjä poistettu."})
+    })
+    
 module.exports = router;
