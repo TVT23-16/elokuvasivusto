a8f0af6fd235dd7ce10dfcdc124e808d25404753
dajaakko <jaakko.kaurala@hotmail.fi>
2024-04-09 21:22:10 +0300

    salasanan hashing tehty

--- server/database/auth_db.js
+++ server/database/auth_db.js
@@ -2,11 +2,17 @@ const pgPool = require ("./pg_connection")
 
 const sql = {
     ADD_USER: "INSERT INTO table_account (password, accountname) VALUES ($1, $2)",
+    GET_PW: "SELECT password FROM table_account WHERE accountname =$1"
 }
 
 async function addUser(password, accountname){
     await pgPool.query(sql.ADD_USER, [password, accountname])
-    
 }
 
-module.exports = {addUser}
+async function getPw(username) {
+    const result = await pgPool.query(sql.GET_PW, [username])
+
+    return result.rowCount > 0 ? result.rows[0].password : null
+}
+
+module.exports = {addUser, getPw}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
-const { addUser } = require("../database/auth_db");
+const { addUser, getPw } = require("../database/auth_db");
 
 const router = require("express").Router();
-
+const bcrypt = require("bcrypt")
 
 
 router.post("/add", async (req,res) => {
@@ -10,14 +10,32 @@ router.post("/add", async (req,res) => {
     const accountname = req.body.accountname
     const password = req.body.password
 
-    
-    await addUser(password, accountname) // Kutsutaan addUser-funktiota uuden käyttäjän lisäämiseksi
+    const hashPw = await bcrypt.hash(password,10)
+    await addUser(hashPw, accountname) // Kutsutaan addUser-funktiota uuden käyttäjän lisäämiseksi
     console.log(req.body.password);
 
     res.end();
    
 })
 
+
+router.post("/login", async (req,res) => {
+    const uname = req.body.accountname
+    const pw = req.body.password
+    const db_pw = await getPw(uname)
+
+    if(db_pw) {
+        const authenticated = await bcrypt.compare(pw, db_pw)
+        if(authenticated){  
+            const token = "asd"
+            res.status(200).json({jwtToken: token})
+        }else{
+            res.status(401).json({error: "Wrong password"})
+        }
+    } else {
+        res.status(404).json({error: "User not found"})
+    }
+})
 
     
 module.exports = router;
