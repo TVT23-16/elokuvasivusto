44408dbb7efec4ed153a6affdc2f6f629ba2d7b0
Miia Alatervo <c2almi00@students.oamk.fi>
2024-04-09 20:43:14 +0300

    Ulkoasua muokattu frontendiin

--- frontend/mooviesite/src/pages/Home.js
+++ frontend/mooviesite/src/pages/Home.js
@@ -1,12 +1,33 @@
-import React from 'react'
-
+import React from 'react';
+import './Home.css';
 
 export default function Home() {
   return (
-    
-    
-      <p>KOTISIVUSTO
-      </p>
-  
-  )
+    <div>
+      <h1>HOT RIGHT NOW</h1>
+
+      <div id="content">
+        <section id="main">
+          <article>
+            <div>
+              <img src="1lehma.jpg" alt="Lehma" class="center"/>
+              <h3>Lehmä 1</h3>
+            </div>
+          </article>
+          <article>
+            <div>
+              <img src="2lehma.jpg" alt="Lehma" class="center"/>
+              <h3>Lehmä 2</h3>
+            </div>
+          </article>
+          <article>
+            <div>
+              <img src="3lehma.jpg" alt="Lehma" class="center"/>
+              <h3>Lehmä 3</h3>
+            </div>
+          </article>
+        </section>
+        </div>
+      </div>
+  );
 }
