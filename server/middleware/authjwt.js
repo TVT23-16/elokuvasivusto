3ee74fb55d3acebadda5f0e7fb3a09809e203d7e
dajaakko <jaakko.kaurala@hotmail.fi>
2024-04-10 19:02:29 +0300

    tehty jwt token middleware ja reviewille tehty addreview route

---
+++ server/middleware/authjwt.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken")
+
+require("dotenv").config()
+
+function authjwt(req, res, next) {
+    const token = req.headers.authorization?.split(" ")[1]
+try{
+    const username = jwt.verify(token,process.env.JWT_SECRET).username
+
+    res.locals.username = username;
+    next();
+} catch(err) {
+    res.status(403),json({error: "access forbidden"})
+    }
+}
+
+module.exports = {authjwt}
