e4f1e587c56050070826b979ddbafa725981e2e5
Miia Alatervo <c2almi00@students.oamk.fi>
2024-04-11 17:39:58 +0300

    Ulkoasua ja toiminnallisuutta muokattu

--- frontend/mooviesite/src/components/Footer.js
+++ frontend/mooviesite/src/components/Footer.js
@@ -5,12 +5,8 @@ import'./Footer.css'
 export default function Footer() {
   return (
     <footer>
-
-      <div className="powered-by">
-      <p>Site powered by</p>
-      </div>
-
       <div className="logo-container">
+      <p>Site powered by</p>
         <a href="https://www.themoviedb.org/" target="_blank">
           <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="Themoviedb logo" width="80px;"/>
         </a>
--- frontend/mooviesite/src/pages/Home.js
+++ frontend/mooviesite/src/pages/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 export default function Home() {
   return (
     <div>
-      <h1>HOT RIGHT NOW</h1>
+      <h1>TOP 3 MOVIES </h1>
 
       <div id="content">
         <section id="main">
@@ -26,8 +26,39 @@ export default function Home() {
               <h3>Lehmä 3</h3>
             </div>
           </article>
+
+          <article>
+            <div>
+              <img src="2lehma.jpg" alt="Lehma" class="center"/>
+              <h3>Lehmä 2</h3>
+            </div>
+          </article>
+          <article>
+            <div>
+              <img src="2lehma.jpg" alt="Lehma" class="center"/>
+              <h3>Lehmä 2</h3>
+            </div>
+          </article>          <article>
+            <div>
+              <img src="2lehma.jpg" alt="Lehma" class="center"/>
+              <h3>Lehmä 2</h3>
+            </div>
+          </article>
         </section>
         </div>
+
+            <div>
+              <h2>Tähän jotain sisältöä</h2>
+              <p>Liibalaba</p>
+              <p>Liibalaba</p>
+              <p>Liibalaba</p>
+              <p>Liibalaba</p>
+              <p>Liibalaba</p>
+              <p>Liibalaba</p>
+            </div>
+
+            <div id="empty-space"></div>
+
       </div>
   );
 }
