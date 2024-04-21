c6e7f11ce3adad97490084888b6681f3236c143b
o7lipy00 <o7lipy00@students.oamk.fi>
2024-04-21 22:00:53 +0300

    moviedetails

--- frontend/mooviesite/src/components/MovieDetail.js
+++ frontend/mooviesite/src/components/MovieDetail.js
@@ -1,12 +1,45 @@
-import React from 'react';
+// MovieDetails.js
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function MovieDetail({ posterUrl, genre, ageRating, language }) {
+function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const apiKey = 'cfaf3af7360c5b3c0549dd08762cb811';
+        const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setMovie(data);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <img src={posterUrl} alt="Movie Poster" />
-      <p>Genre: {genre}</p>
-      <p>Age Rating: {ageRating}</p>
-      <p>Language: {language}</p>
+      <h1>{movie.title}</h1>
+      <p>Overview: {movie.overview}</p>
+      <p>Release Date: {movie.release_date}</p>
+      <p>Rating: {movie.vote_average}</p>
+      {movie.poster_path && (
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      )}
     </div>
   );
 }
+
+export default MovieDetails;
