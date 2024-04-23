7b712a44cc8cb62804b313977a9772f9b9da6561
Miia Alatervo <c2almi00@students.oamk.fi>
2024-04-23 14:26:19 +0300

    dropdownia ja navbaria korjattu, ulkoasuja ja kieliä vaihdettu

--- frontend/mooviesite/src/components/DropdownMenu.js
+++ frontend/mooviesite/src/components/DropdownMenu.js
@@ -1,38 +1,59 @@
-import React, { useState,} from 'react';
-import { Link, useLinkClickHandler } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './DropdownMenu.css';
-import Myprofile from '../pages/Myprofile';
-
+import { useLanguage } from '../LanguageContext';
 
 const DropdownMenu = ({ user }) => {
-  const [isOpen, setisOpen] = useState(false);
-  const toggleDropdown =()=> {setisOpen(!isOpen)}
-  return (
+  const [isOpen, setIsOpen] = useState(false);
+  const { language } = useLanguage();
 
-       <div id="profile.png">
-       
-      <button className="dropdown-toggle" onClick={toggleDropdown} >
-      <img src='./profile.png' alt="profilepic" ></img>
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsOpen(false); // Tämä funktio sulkee dropdown-valikon
+  };
+
+  useEffect(() => {
+    // Kuuntelija, joka sulkee dropdown-valikon, kun käyttäjä klikkaa hiirellä muualle sivustolla
+    const handleClickOutside = (event) => {
+      if (event.target.closest('.dropdown-menu-container') === null) {
+        setIsOpen(false);
+      }
+    };
+
+    // Lisää tapahtumakuuntelija kun komponentti mountataan
+    window.addEventListener('click', handleClickOutside);
+
+    // Poista tapahtumakuuntelija kun komponentti unmountataan
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []); // Tyhjä taulukko varmistaa, että useEffect suoritetaan vain kerran, kun komponentti mountataan
+
+  return (
+    <div className="dropdown-menu-container">
+      <button className="dropdown-toggle" onClick={toggleDropdown}>
+        <img src='/profile.png' alt="profilepic" />
       </button>
-      
       {isOpen && (
         <ul className="dropdown-menu">
-         
           <li>
-            <Link to="/myprofile/delete">Delete account</Link>
-
+            {user === null ? (
+              <Link to="/login" onClick={closeDropdown}>{language === 'ENG' ? 'LOGIN' : 'KIRJAUDU SISÄÄN'}</Link>
+            ) : (
+              <>
+                <Link to="/myprofile" onClick={closeDropdown}>{language === 'ENG' ? 'My profile' : 'Profiili'}</Link>
+                <Link to="/myprofile/delete" onClick={closeDropdown}>{language === 'ENG' ? 'Delete account' : 'Poista käyttäjä'}</Link>
+                <Link to="/Logout" onClick={closeDropdown}>{language === 'ENG' ? 'Logout' : 'Kirjaudu ulos'}</Link>
+              </>
+            )}
           </li>
-          <li>
-            <Link to="/Logout">Logout</Link>
-          <li>
-          </li>
-          <Link to="/myprofile">My profile</Link>
-          </li>
-          
         </ul>
       )}
     </div>
-)
+  );
 };
 
 export default DropdownMenu;
