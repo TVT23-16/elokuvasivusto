1bf10d97e19c769947366171c599cba9b0a1e33a
Miia Alatervo <c2almi00@students.oamk.fi>
2024-04-25 14:39:03 +0300

    Tähtiarvostelua yritetty

--- frontend/mooviesite/src/components/SerieDetail.js
+++ frontend/mooviesite/src/components/SerieDetail.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './MovieDetail.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useLanguage } from '../LanguageContext';
+import { FaStar } from 'react-icons/fa';
 
 function SeriesDetail({ user }) {
   const { id } = useParams();
@@ -10,10 +11,13 @@ function SeriesDetail({ user }) {
   const [UserReview, setUserReview] = useState("");
   const [Result, setResult] = useState([]);
   const { language } = useLanguage(); // Hae nykyinen kieli
+  const [hover, setHover] = useState(null);
+  const navigate = useNavigate();
+  const [rating, setRating] = useState(null);
 
   useEffect(() => {
     if (user) {
-      const { user: username } = user; // Destructure username from user object
+      const { user: username } = user;
       setUname(username);
       console.log(username);
     }
@@ -33,7 +37,7 @@ function SeriesDetail({ user }) {
     };
 
     fetchSeriesDetails();
-  }, [id]);
+  }, [id, language]);
 
   useEffect(() => {
     if (series) {
@@ -73,11 +77,12 @@ function SeriesDetail({ user }) {
           media_id: id,
           userreview: UserReview,
           accountname: uname,
-          stars: "5"
+          stars: rating
         }),
       })
       if (response.ok) {
-        setResult([...Result, { userreview: UserReview, accountname: uname }]); //...result tekee kopion result taulukosta ja lisää sinne suoraan uuden arvostelun
+        setResult([...Result, { userreview: UserReview, accountname: uname }]);
+        setRating(null);
       }
 
     } catch (error) {
@@ -92,8 +97,15 @@ function SeriesDetail({ user }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (rating === null) {
+      alert(language === 'ENG' ? 'Please select stars before submitting your review!' : 'Valitse tähdet ennen kuin lähetät arvostelun!');
+
+      return;
+    }
     addReview();
+    setUserReview("");
   };
+  
 
   return (
     <div className="movie-details-container">
@@ -105,33 +117,71 @@ function SeriesDetail({ user }) {
             <p className='p-overview'>First Air Date: {series.first_air_date}</p>
             <p className='p-overview'>Rating: {series.vote_average}</p>
             <h2>{language === 'ENG' ? 'Leave rating' : 'Jätä arvostelu'}</h2>
+
             <form onSubmit={handleSubmit}>
               <div className='writeRating'>
-                <textarea name="postContent" rows={4} cols={40} value={UserReview} onChange={handleuserReview} className="postContent" />
+                {[...Array(5)].map((star, index) => {
+                  const currentRating = index + 1;
+                  return (
+                    <div key={index} className='star-container'>
+                      <FaStar
+                        className='star'
+                        size={50}
+                        color={currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                        onMouseEnter={() => setHover(currentRating)}
+                        onMouseLeave={() => setHover(null)}
+                        onClick={() => setRating(currentRating)}
+                      />
+                    </div>
+                  );
+                })}
+                <p>
+                  {language === 'ENG'
+                    ? rating
+                      ? rating === 1
+                        ? 'Your rating: 1 star'
+                        : `Your rating: ${rating} stars`
+                      : 'Add stars to your rating'
+                    : rating
+                      ? rating === 1
+                        ? 'Arviosi: 1 tähti'
+                        : `Arviosi: ${rating} tähteä`
+                      : 'Lisää tähdet arvosteluusi'}
+                </p>
+
+                <textarea name="postContent" rows={4} cols={40} value={user ? UserReview : "Kirjaudu sisään kirjoittaaksesi arvostelun"} onChange={handleuserReview} className="postContent" />
                 <button type='submit'>{language === 'ENG' ? 'Submit' : 'Lähetä'}</button>
               </div>
             </form>
-            </div>
-            {series.poster_path && (
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${series.poster_path}`}
-                alt={series.name}
-                className="moviedetail-pic"
-              />
-            )}
-            </div>
-            <h2>{language === 'ENG' ? 'Other users reviews' : 'Muiden käyttäjien arvostelut'}</h2>
-            <div className="reviews">
-              {Result.length > 0 && (
-                Result.map((review, index) => (
-                  <label key={index}>
-
-                    <textarea value={"Reviewer:" + review.accountname + "\n\n" + review.userreview} rows={4} cols={40} readOnly className="postContent" />
-
-                  </label>
-                ))
-              )}
+          </div>
+          {series.poster_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${series.poster_path}`}
+              alt={series.name}
+              className="moviedetail-pic"
+            />
+          )}
         </div>
+      </div>
+      <h2>{language === 'ENG' ? 'Other users reviews' : 'Muiden käyttäjien arvostelut'}</h2>
+      <div className="reviews">
+      {Result.length > 0 && (
+  Result.map((review, index) => (
+    <label key={index}>
+     <textarea 
+  value={`${
+    language === 'ENG' ? 'Reviewer' : 'Arvostelija'
+  } ${review.accountname}\n\n${
+    language === 'ENG' ? 'Stars' : 'Tähdet'
+  }: ${review.stars}\n\n${review.userreview}`} 
+  rows={4} 
+  cols={40} 
+  readOnly 
+  className="postContent" 
+/>
+    </label>
+  ))
+)}
       </div>
     </div>
   );
